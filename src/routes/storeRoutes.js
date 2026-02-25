23c6b8adcfb81f8733bf418cb1704ef294ad8384
uttam <uttamgupta628@gmail.com>
2026-02-25 11:06:39 +0530

    scan controller

--- src/routes/storeRoutes.js
+++ src/routes/storeRoutes.js
@@ -14,7 +14,6 @@ const {
   getFavoriteStores,
   getFavoriteStoresByUserId,
   getNearbyStores,
-  a,
 } = require("../controllers/storeController");
 const { authenticate } = require("../utils/auth");
 
@@ -33,18 +32,3 @@ router.get("/favorites/:user_id", authenticate, getFavoriteStoresByUserId);
 router.get("/nearby", authenticate, getNearbyStores);
 
 module.exports = router;
-// const express = require("express");
-// const {
-//   createStore,
-//   getStore,
-//   updateStore,
-// } = require("../controllers/storeController");
-// const { authenticate } = require("../utils/auth");
-
-// const router = express.Router();
-
-// router.post("/", authenticate, createStore);
-// router.get("/", authenticate, getStore);
-// router.patch("/", authenticate, updateStore);
-
-// module.exports = router;
